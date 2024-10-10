993dbc1b35f59ef51e8b40ebca82bd3607c774be
Tigo-cmd <ja16162040@gmail.com>
2024-10-10 22:51:43 +0100

    [Add] requirements

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""concat annotations implementation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+
+    :param str1: first string
+    :param str2: second string
+    :return: concatenated string of both variables
+    """
+    return f"{str1} {str2}"
