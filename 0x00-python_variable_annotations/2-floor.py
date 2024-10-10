96cfe9f66f359fa627babc50ea3ba43d66d7aaa2
Tigo-cmd <ja16162040@gmail.com>
2024-10-10 23:08:08 +0100

    [Add] implemented Task 2 #sucess

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this module implements an annotated function"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    this function returns a floored float typically converts it to integer
+    :param n: float to be converted
+    :return: the floored int
+    """
+    return math.floor(n)
