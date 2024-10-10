5d11bede68bbe79fdd1ce6f6dbac2dcc76aacfbe
Tigo-cmd <ja16162040@gmail.com>
2024-10-10 21:56:46 +0100

    [Add] implemented an annotated func

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""basic Annotations implementations"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    this function takes two float variable and return their sum
+    :param a: first float number
+    :param b: second float number
+    :return: sum of the two arguments
+    """
+    return a * b
