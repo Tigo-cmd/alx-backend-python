410f2abc3b77a4660446e39d087f6b250d4f6d33
Tigo-cmd <ja16162040@gmail.com>
2024-10-10 23:12:35 +0100

    [Add] implemented Task 3 #sucess

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""this module implements a tyoe annotated function"""
+
+
+def to_str(n: float) -> str:
+    """
+    return the string repr of a given float
+    :param n: float input
+    :return: string representation
+    """
+    return str(n)
