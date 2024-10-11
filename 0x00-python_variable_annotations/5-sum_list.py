538e020d3eee25b9b5ffbe3690e31defb4a2dc85
Tigo-cmd <ja16162040@gmail.com>
2024-10-11 02:18:13 +0100

    [Add] implemented Task 3 #sucess

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,9 +8,4 @@ def sum_list(input_list: list[float]) -> float:
     :param input_list: list of floats to be inputted
     :return: sum of floats
     """
-    # sumlist: int = 0
-    # for i in input_list:
-    #     sumlist += i
-    # return sumlist     ...this works fine but i just want an error free implelmentation
-
     return sum(input_list)
