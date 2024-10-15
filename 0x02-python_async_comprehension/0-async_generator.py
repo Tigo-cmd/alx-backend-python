e8f93bba5251e40fcc02ab5e83bc4c5bf2c82a20
Tigo-cmd <ja16162040@gmail.com>
2024-10-15 12:32:05 +0100

    [Fix] random floating point for generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import asyncio
 
 
 async def async_generator() -> Generator[int, None, None]:
-    rand_num = uniform(0.0, 10)
+    rand_num = uniform(0, 10)
     for _ in range(10):
         await asyncio.sleep(1)
         yield rand_num
